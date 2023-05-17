3ea079e2111b9c6a86b1fad453844d41e0f0d103
Piyush Ahir <egpiyushahir@gmail.com>
2023-05-18 01:28:12 +0530

    fix - added home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
-
+import { Home } from "./components/Home/Home";
+import Mockman from "mockman-js";
 import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
