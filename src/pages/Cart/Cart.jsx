cf7bfb7b6f78c99a0e2fdb6d1c078be4a8556623
Piyush Ahir <egpiyushahir@gmail.com>
2023-06-21 15:28:52 +0530

    feat - cart update functionality

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -3,9 +3,25 @@ import { Footer } from "../../components/Footer/Footer";
 import "./Cart.css";
 import { DataContext } from "../../context/DataContext";
 import { ProductCard } from "../../components/ProductCard/ProductCard";
+import { useNavigate } from "react-router-dom";
 
 export const Cart = () => {
   const { state } = useContext(DataContext);
+  const navigate = useNavigate();
+  if (state?.cart?.length === 0) {
+    return (
+      <>
+        <div className="none-container">
+          <h2>Oops! Nothing in Cart</h2>
+          <div>
+            <button onClick={() => navigate("/products")}>
+              Explore Products
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  }
   return (
     <>
       <div className="cart">
